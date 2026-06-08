6f499ec40c5450aae548ff1474f2b4b183f59323
aRobert <124544863+RobertFilms@users.noreply.github.com>
2026-06-08 15:32:31 -0400

    fixed spelling

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const resumeData = {
             description: 'Provide technical support and troubleshooting for clients using the company’s software products, ensuring timely resolution of issues and maintaining high customer satisfaction through clear communication and effective problem-solving.'
         },
         {
-            title: 'Starbuck Barista & Checkout Associate',
+            title: 'Starbucks Barista & Checkout Associate',
             company: 'Giant Food Stores',
             duration: 'Aug 2025 - June 2026',
             description: 'Worked in a fast-paced environment, providing excellent customer service, managing transactions, and collaborating with team members to ensure smooth operations during peak hours.'
